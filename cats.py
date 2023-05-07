5022ae6cbe6ece341f06b20ac3694c6a8edcd491
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-05-07 12:32:45 +0200

    added the initial cats.py

---
+++ cats.py
@@ -0,0 +1,78 @@
+catRarity = [("common", 50), ("uncommon", 25), ("rare", 12), ("epic", 6), ("legendary", 3)]
+catpack = {
+    "common": [
+        ["https://tenor.com/view/kitty-cat-sandwich-cats-sandwich-gif-26112528", 'Sandwich Cat', "mmm yummy :O.."],
+        ["https://tenor.com/view/byuntear-sad-sad-cat-cat-meme-gif-25617057", 'Soggy Cat', "He's so wet :O"],
+        ["https://tenor.com/view/jinx-the-cat-jinx-jinx-cat-cat-computer-gif-25786466", 'Laptop Cat', "He likes looking at computers."],
+        ["https://tenor.com/view/meme-cat-gif-23774444", 'Disappointed Cat', "He's kinda disappointed \nidk about what tho ¯\_(ツ)_/¯ \nask him..."],
+        ["https://cdn.discordapp.com/attachments/1089526107096756384/1089820636186153020/Z.png", 'Smiley Cat', "hes so happy!!! :D"],
+        ["https://tenor.com/view/banana-cat-eating-gif-18642422", 'Banana Gulper', "hes eatin' that shit up dawg..."],
+        ["https://tenor.com/view/kitty-24kse0k-gif-23131783", 'The Stumbler', "he's on the drugs..."],
+        ["https://media.discordapp.net/attachments/852784097897086996/1075896795852443669/IMG_2106.gif", 'The Aggressive Petter', "Who did this to him..."],
+        ["https://media.discordapp.net/attachments/778358206920392764/879212536455639050/image0-5-1-1.gif", 'The Bottler', "He's gonna kill you \nfor that..."],
+        ["https://cdn.discordapp.com/attachments/1089526107096756384/1089944744781680721/IMG_0816.jpg", 'The Biter', "why he bite :("],
+        ["https://tenor.com/view/cat-gif-19244314", 'The Partygoer', "He's so tired\ntoo much partying ig..."],
+        ["https://media.discordapp.net/attachments/951827705609990174/952146937476427786/SPOILER_BF5CD75A-468E-472A-975E-5CA9CC0ED646.gif", 'Soapy Cat', "He's clean."]
+    ],
+
+    "uncommon": [
+        ["https://media.discordapp.net/attachments/926624188335267870/1084528896550109325/Fq8mcFLWwAEy6Lq.gif", '*THE ANGER 1*', "grrrrrr....."],
+        ["https://cdn.discordapp.com/attachments/1089526147588558948/1089850898169208884/image.png", '*THE ANGER 2*', "grrrrrrrrr....."],
+        ["https://media.discordapp.net/attachments/1062270975141498922/1073883690750464050/uncaption-2-1.gif", 'Confused Cat', "Huh? Wuh? Whah???"],
+        ["https://tenor.com/view/cute-cat-gif-23325548", 'Approaching Cat', "Here he comes...."],
+        ["https://tenor.com/view/cat-bed-laying-lazy-dzekas-gif-20013760", 'Laying Cat', "He needs a therapist."],
+        ["https://media.discordapp.net/attachments/861243804295364639/1074855569468436530/ezgif.com-gif-maker.gif", 'Silly Cat', "bleghgghhh"],
+        ["https://tenor.com/view/cat-snore-drool-gif-24001576", 'Sleepy Cat', "schhh wiwiwiwiiwiw"],
+        ["https://tenor.com/view/elisttm-cat-funny-eyes-bounce-gif-20657317", 'Bouncey Cat', "Why does he allow this...."],
+        ["https://tenor.com/view/oh-no-cute-cat-gif-15060173", 'No Cat?', "He's shaking no."], 
+        ["https://media.discordapp.net/attachments/1081733926353108995/1090799182295613440/IMG_8080.gif", 'The Sniffer Cat', "Powerpoint presentation:"], 
+        ["https://media.discordapp.net/attachments/929807268994744340/1062457104587313162/BDC73095-4C85-4D17-A342-17734233F46E.gif", 'Petted Speachbubble Cat', "oyuughh feels good"],
+        ["https://tenor.com/view/michael-pipe-bomb-bomb-mail-box-cat-gif-18431261", 'PIPEBOMB Cat', "Check your mailbox :)"],
+        ["https://media.discordapp.net/attachments/923861757854883871/1018836290009055312/20210401_133622.gif", 'Vibrator Cat', "wrrrrrrrrr"],
+    ],
+
+    "rare": [
+        ["", '', ""],
+    ],
+
+    "epic": [
+        ["https://media.discordapp.net/attachments/1053450743773937695/1087659118749286480/365156F1-97DC-4926-998E-AF33F76006AE.gif", 'Attention Cat', "He wants your attention.."],
+        ["https://media.discordapp.net/attachments/1030635973119320125/1080890978501664808/7ca77867dd5e85b2.gif", 'Campfire Cats', "🎵 Chilling around the campfire 🎵"],
+        ["https://media.discordapp.net/attachments/926624188335267870/1081298898439651468/received_606139214688728.gif", 'Besties', "They are besties!!"],
+        ["https://tenor.com/view/cat-standing-fat-epic-gif-23115386", 'Plus Sized Maxwell.', "You overfed him."],
+        ["https://cdn.discordapp.com/attachments/1089526186880806965/1089828708170346528/IMG_4618.png", 'Glenn', "Glenn loves macaroni night"],
+        ["https://media.discordapp.net/attachments/1033857132463927327/1077446533257105528/FRM_Jc6X0AEQzQ-.gif", 'Scrolling Cat', "Scrollllllllllllllllllllllllllllllllllllllllllllllllllll"],
+        ["https://media.discordapp.net/attachments/1026607313630548008/1073802380589666445/ezgif-7-042e77fcab2d.gif", 'Flappy Cat', "flwp flwp flwp"],
+        ["https://media.discordapp.net/attachments/1049251991605477377/1073331684549853325/2A366F3D-A4B9-4D1D-97CB-61E341C3C463.gif", 'Shlerping cat', "slurpppp"],
+        ["https://images-ext-2.discordapp.net/external/rOjkQ8ahEaeLL-U0PX4iUuLzRqs1DdvhoC2Ca2j_rKk/https/media.tenor.com/1SMrekR7KgQAAAPo/cat-angry.mp4", 'Hapi Hapi', "Hapi Hapi"],
+        ["https://tenor.com/view/sad-cat-sunakook-tired-exhausted-gif-24687868", 'Sitting Cat', "He's tired..."],
+        ["https://media.discordapp.net/attachments/914872564147449876/1050371407097364601/14FE06B0-4528-465A-9000-78B2F69F6B5B.gif", 'Vibe Blob', "woooooooooooooooooooooooooooooooooo"],
+        ["https://tenor.com/view/cat-do-not-dont-please-meme-gif-21449752", "Do not the Cat.", "Do not the Cat."],
+        ["https://tenor.com/view/oh-hes-not-happy-cat-glitch-cat-yell-cat-pet-gif-24454033", "Screaming Cat", "AAAAAAAAAAAAAAAAAAAAAAAEEAEEoeeoeoaese"],
+        ["https://tenor.com/view/cat-vape-juul-gif-19293187", "Vaping Cat", "he blowsss that shit"],
+        ["https://tenor.com/view/mr-hakan-gif-18580550", "Cat getting faded", "snip snip"],
+        ["https://cdn.discordapp.com/attachments/1089526186880806965/1093552105303842866/FrRBdonX0AQ4-ug.jpg", "Held Cat", "He's being held."],
+        ["https://cdn.discordapp.com/attachments/1089526186880806965/1093552173415145533/FqVTmhYWAAUptqf.png", "NOT!! Workout Cat", "to sleepy. no gains. on the ground."],
+        ["https://cdn.discordapp.com/attachments/1089526186880806965/1093552186123890818/caption-5.gif", ":3", ":3"],
+        ["https://tenor.com/view/shaking-head-cat-metal-mental-gif-25946742", "THE DESTROYER", "R.I.P to the couch."],
+    ],
+
+    "legendary": [
+        ["", '', ""],
+    ]
+}
+
+buttpack = [
+    ["https://tenor.com/view/but-heres-the-jumper-cat-gif-26465799", "jumper", ""],
+    ["https://tenor.com/view/but-heres-the-launcher-cat-gif-26465802", "launcher", ""],
+    ["https://tenor.com/view/but-heres-the-uh-i-forgor-gif-26494840", "forgor", ""],
+    ["https://tenor.com/view/but-heres-the-puncher-cat-gif-26465800", "puncher", ""],
+    ["https://tenor.com/view/flyer-the-heres-but-polynomers-gif-26406778", "flyer", ""],
+    ["https://tenor.com/view/but-heres-the-walker-cat-gif-26557600", "walker", ""],
+    ["https://tenor.com/view/but-heres-the-kicker-gif-26137308", "kicker", ""],
+    ["https://tenor.com/view/but-heres-the-sadder-cat-gif-26465773", "sadder", ""],
+    ["https://tenor.com/view/but-heres-the-licker-cat-gif-26550624", "licker", ""],
+    ["https://tenor.com/view/but-heres-the-screamer-cat-gif-26604710", "screamer", ""],
+    ["https://tenor.com/view/but-heres-the-closer-polynomers-gif-26426427", "closer", ""],
+    ["https://tenor.com/view/but-heres-the-cat-cat-swim-but-heres-the-swimmer-gif-27014997", "swimmer", ""],
+]
